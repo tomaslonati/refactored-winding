f09d1c48ac0eb3bd60429a37cf3cf140c48c05ed
Tomás <tuemail@example.com>
2025-05-13 12:23:32 -0300

    agregando rutas

---
+++ my-react-app/src/components/pages/home.jsx
@@ -0,0 +1,13 @@
+import Products from '../pages/products.jsx'
+
+function Home() {
+    return (
+        <div className="w-full">
+            <h1>home</h1>
+            <Products />
+            <p>ss</p>
+        </div>
+    );
+}
+
+export default Home;
---
+++ my-react-app/src/components/pages/products.jsx
@@ -0,0 +1,11 @@
+
+function Products() {
+    return (
+        <div className="w-full">
+            <p>aerogenerador</p>
+            <p>EOLICO</p>
+        </div>
+    );
+}
+
+export default Products;
